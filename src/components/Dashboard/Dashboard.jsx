00c63140a8d1c5485eebe4d0afe6487533549410
Phi Anh <phianh5@gmail.com>
2023-05-25 21:54:09 +0700

    chart

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -11,6 +11,7 @@ import {
   TableRow,
   Typography,
 } from '@mui/material';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts';
 import React, { useState } from 'react';
 import Chart from '../Chart/Chart';
 import InputLabel from '@mui/material/InputLabel';
@@ -20,7 +21,53 @@ import Link from '@mui/material/Link';
 import { REPORT_CRITERIA, URL_SIDEBAR } from '../../utils/globalVariables';
 import { useNavigate } from 'react-router-dom';
 import { useGetAllOrdersQuery } from '../../services/orderApis';
-import { useGetProfitByCriteriaQuery } from '../../services/reportApis';
+import { useGetProfitByCriteriaQuery, useGetSoldStatisticQuery } from '../../services/reportApis';
+
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 const Dashboard = () => {
   const [reportCriteria, setReportCriteria] = useState(REPORT_CRITERIA.day);
@@ -31,6 +78,11 @@ const Dashboard = () => {
   const { data: ordersData, isFetching: isFetchingOrdersData } =
     useGetAllOrdersQuery();
   console.log(ordersData);
+
+  const { data: soldData, isFetching: isFetchingSoldData } =
+  useGetSoldStatisticQuery();
+
+  console.log(soldData);
 
   const navigate = useNavigate();
 
@@ -63,6 +115,7 @@ const Dashboard = () => {
                 height: 240,
               }}
             >
+              
               <Stack
                 direction="row"
                 justifyContent="flex-start"
@@ -105,6 +158,39 @@ const Dashboard = () => {
               </Typography> */}
             </Paper>
           </Grid>
+          {isFetchingSoldData ? (<></>) : (<Grid item xs ={12}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 450,
+                width: 1100,
+              }}
+            >
+              <Typography component="p" variant="body1" fontSize={20} mb={1}>
+                Số lượng sản phẩm đã bán
+              </Typography>
+              <BarChart
+                width={1000}
+                height={400}
+                data={soldData}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name"></XAxis>
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="sold" fill="#82ca9d" />
+        </BarChart>
+            </Paper></Grid>)}
+          
           {/* Recent Orders */}
           <Grid item xs={12}>
             <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
@@ -241,7 +327,9 @@ const Dashboard = () => {
               )}
             </Paper>
           </Grid>
+          
         </Grid>
+        
         <Typography
           mt={3}
           variant="body2"
